a9b367000cb7fc76d535308a04a1f2e87f97ca20
bijaykarki <bjkarki10@gmail.com>
2022-10-08 02:50:24 +0300

    feat: few visual corrections made to the main file.

---
+++ introductory_text.py
@@ -0,0 +1,34 @@
+def gameDescription():
+    print()
+    print(
+        "-----------------------------------Welcome to 'A Flight Simulator game'----------------------------------------")
+    print(
+        "-----------------------------------------Designed by 'Group 4'-------------------------------------------------")
+    print()
+    print("The game consists of three stages.")
+    print()
+    print(
+        ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    print("Stage 1: The game asks 'name' and 'starting location' (in ICAO format) of the player.")
+    print()
+    print(
+        ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    print()
+    print("Stage 2: The player will be flying an electric plane but the battery is empty in the beginning.")
+    print("         So, the player must play a quiz game to earn battery life.")
+    print("         The player chooses a topic of his/her choice out of 5, each containing 5 questions.")
+    print("         For each right answer, 2500 kAmph battery life is rewarded to the player.")
+    print()
+    print(
+        ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    print()
+    print("Stage 3: After earning battery life, the player is ready to fly around the world.")
+    print(
+        "         The goal of the player is to collect weather passports by flying around the world within battery life.")
+    print("         The weather passport are; 'HOT, COLD, 0 DEG, 10 DEG, 20 DEG, CLEAR, CLOUDS, WINDY'")
+    print(
+        "         The player does not have to pick the passport in advanced and can be collected in any order!")
+    print()
+    print(
+        ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    print()
