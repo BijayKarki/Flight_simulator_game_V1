298706bf483d8eedd2728df0569bbdb7b617b231
bijaykarki <bjkarki10@gmail.com>
2022-10-12 02:18:20 +0300

    ft: as player plays the game, game table in the db is updated.

---
+++ game_service.py
@@ -0,0 +1,40 @@
+from Database_Connection import connection
+
+
+def get_game_by_id(id):
+    sql_read = f"SELECT * FROM game WHERE id = '{id}'"
+
+    cursor = connection.cursor()
+    cursor.execute(sql_read)
+    result = cursor.fetchone()
+
+    return result
+
+
+def create_game(screen_name, co2_budget, location):
+    import uuid
+    unique_id = str(uuid.uuid4())[:8]  # unique identifier
+    # print(str(unique_id)[:8])  # converting it into a string and slicing only 8 characters
+
+    sql_write = f"INSERT INTO game(id, screen_name, co2_budget, location) " \
+                f"VALUES('{unique_id}', '{screen_name}', '{co2_budget}', '{location}')"
+
+    cursor = connection.cursor()
+    cursor.execute(sql_write)
+
+    return unique_id
+
+
+def update_game_by_id(id, co2_consumed, location):
+    sql_update = f"UPDATE game SET co2_consumed = '{co2_consumed}', location ='{location}' WHERE id = '{id}'"
+
+    cursor = connection.cursor()
+    cursor.execute(sql_update)
+    # result = cursor.fetchall()
+
+
+game_id = create_game('test55', 10000, 'EFHK')
+
+print(game_id)
+
+update_game_by_id(game_id, 5000, 'EECL')
---
+++ weather_goals.py
@@ -0,0 +1,37 @@
+from Weather_API import getWeatherDataByLatLon
+
+# Helsinki Vantaa Airport (60.3172, 24.963301)
+
+weather_values = getWeatherDataByLatLon(60.3172, 24.963301)
+
+# goal_targets = ['HOT', 'COLD', '0DEG', '10DEG', '20DEG', 'CLEAR', 'CLOUDS', 'WINDY']
+goal_targets = {0: 'HOT', 1: 'COLD', 2: '0DEG', 3: '10DEG', 4: '20DEG', 5: 'CLEAR', 6: 'CLOUDS', 7: 'WINDY'}
+goal_achieved = set()
+print()
+print(f"Goal targets set:{goal_targets}.")
+print()
+
+# conditions for temperature values
+if weather_values[0] > 25:
+    goal_achieved.add(goal_targets[0])
+elif weather_values[0] < -20:
+    goal_achieved.add(goal_targets[1])
+elif weather_values[0] == 0:
+    goal_achieved.add(goal_targets[2])
+elif weather_values[0] == 10:
+    goal_achieved.add(goal_targets[3])
+elif weather_values[0] == 20:
+    goal_achieved.add(goal_targets[4])
+
+# condition for clear or cloudy sky
+if weather_values[2] == 0:
+    goal_achieved.add(goal_targets[5])
+else:
+    goal_achieved.add(goal_targets[6])
+
+# conditions for wind speed
+if weather_values[1] > 10:
+    goal_achieved.add(goal_targets[7])
+
+print(f'Achieved goals are: {goal_achieved}')
+print(f'Remaning goals are: {goal_targets}')
